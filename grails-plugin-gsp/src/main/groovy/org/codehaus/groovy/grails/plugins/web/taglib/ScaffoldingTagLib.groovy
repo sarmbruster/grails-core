26d7e997684534aefbca393725c04a4aa7f2001f
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-06-20 13:06:40 +0100

    passing test for config and local template overrides

--- grails-plugin-gsp/src/main/groovy/org/codehaus/groovy/grails/plugins/web/taglib/ScaffoldingTagLib.groovy
+++ grails-plugin-gsp/src/main/groovy/org/codehaus/groovy/grails/plugins/web/taglib/ScaffoldingTagLib.groovy
@@ -2,12 +2,12 @@ package org.codehaus.groovy.grails.plugins.web.taglib
 
 import grails.artefact.Artefact
 import org.codehaus.groovy.grails.commons.GrailsApplication
-import org.codehaus.groovy.grails.io.support.GrailsResourceUtils
+import org.codehaus.groovy.grails.plugins.support.aware.GrailsApplicationAware
 import org.codehaus.groovy.grails.web.taglib.exceptions.GrailsTagException
 
 @Artefact("TagLibrary")
-class ScaffoldingTagLib {
-	
+class ScaffoldingTagLib implements GrailsApplicationAware {
+
 	GrailsApplication grailsApplication
     def groovyPagesTemplateEngine
 
@@ -21,14 +21,17 @@ class ScaffoldingTagLib {
 		def model = [property: property, value: value]
 		
 		def type = getPropertyType(bean, property)
-		def template = grailsApplication.config.scaffolding.template.default[type]
+        println "In tag: ${grailsApplication.config}"
+		def template = grailsApplication.config.scaffolding.template.default[type.name]
+        println "template from config: $template"
 		if (!template) {
-			// attempt to use a template in the controller's view directory
-			template = property
-		}
+            // attempt to use a template in the controller's view directory
+            template = property
+            println "template from property name: $template"
+        }
 	
 		try {
-			out << render(template: property, bean: bean, model: model)
+			out << render(template: template, bean: bean, model: model)
 		} catch (GrailsTagException e) {
 			out << textField(name: property, value: value)
 		}
--- grails-test-suite-web/src/test/groovy/org/codehaus/groovy/grails/web/taglib/ScaffoldingTagLibTests.groovy
+++ grails-test-suite-web/src/test/groovy/org/codehaus/groovy/grails/web/taglib/ScaffoldingTagLibTests.groovy
@@ -1,17 +1,15 @@
 package org.codehaus.groovy.grails.web.taglib
 
-import org.junit.*
-import org.codehaus.groovy.grails.support.*
-import org.codehaus.groovy.grails.web.taglib.*
-import org.codehaus.groovy.grails.web.taglib.exceptions.*
+import org.codehaus.groovy.grails.support.MockStringResourceLoader
+import org.codehaus.groovy.grails.web.taglib.exceptions.GrailsTagException
 
 class ScaffoldingTagLibTests extends AbstractGrailsTagTests {
-	
-	def personInstance
-	def resourceLoader
-	
-	protected void onSetUp() {
-		gcl.parseClass '''
+
+    def personInstance
+    def resourceLoader
+
+    protected void onSetUp() {
+        gcl.parseClass '''
 			@grails.persistence.Entity
 			class Person {
 				String name
@@ -20,60 +18,62 @@ class ScaffoldingTagLibTests extends AbstractGrailsTagTests {
 				Date dateOfBirth
 			}
 		'''
-	}
-	
-	void setup() {
-		super.setUp()
-		
-        def domain = ga.getDomainClass("Person")
-		
+    }
+
+    @Override
+    void setUp() {
+        super.setUp()
+
         resourceLoader = new MockStringResourceLoader()
         appCtx.groovyPagesTemplateEngine.resourceLoader = resourceLoader
 
         webRequest.controllerName = "person"
 
-		personInstance = domain.newInstance(name: "Bartholomew Roberts", password: "BlackBart", gender: "Male", dateOfBirth: new Date(1682, 4, 17))
-	}
+        def domain = ga.getDomainClass("Person")
+        personInstance = domain.clazz.newInstance(name: "Bartholomew Roberts", password: "BlackBart", gender: "Male", dateOfBirth: new Date(-218, 4, 17))
+    }
 
-	void testBeanAttributeIsRequired() {
-		shouldFail(GrailsTagException) {
-			applyTemplate('<g:scaffoldInput property="name"/>')
-		}
-	}
+    void testBeanAttributeIsRequired() {
+        shouldFail(GrailsTagException) {
+            applyTemplate('<g:scaffoldInput property="name"/>')
+        }
+    }
 
-	void testPropertyAttributeIsRequired() {
-		shouldFail(GrailsTagException) {
-			applyTemplate('<g:scaffoldInput bean="${personInstance}"/>', [personInstance: personInstance])
-		}
-	}
-	
-	void testResolvesTemplateFromControllerViewsDirectory() {
+    void testPropertyAttributeIsRequired() {
+        shouldFail(GrailsTagException) {
+            applyTemplate('<g:scaffoldInput bean="${personInstance}"/>', [personInstance: personInstance])
+        }
+    }
+
+    void testResolvesTemplateFromControllerViewsDirectory() {
         resourceLoader.registerMockResource("/person/_password.gsp", '<input type="password" name="${property}" value="${value}">')
-		
-		def output = applyTemplate('<g:scaffoldInput bean="${personInstance}" property="password"/>', [personInstance: personInstance])
-		
-		assert output == '<input type="password" name="password" value="BlackBart">'
-	}
-	
-	// TODO: test for template provided by a plugin
-	
-	void testResolvesConfiguredTemplateForDomainClassProperty() {
-		
-	}
-	
-	void testResolvesConfiguredTemplateForPropertyType() {
-        resourceLoader.registerMockResource("/templates/_date.gsp", '<input type="date" name="${property}" value="${formatDate(date: value format=\'yyyy-MM-dd\')}">')
-		ga.config.scaffolding.template.default.java.util.Date = "/templates/date"
-		
-		def output = applyTemplate('<g:scaffoldInput bean="${personInstance}" property="dateOfBirth"/>', [personInstance: personInstance])
-		
-		assert output == '<input type="date" name="dateOfBirth" value="1682-05-17">'
-	}
-	
-	void testUsesDefaultRendering() {
-		def output = applyTemplate('<g:scaffoldInput bean="${personInstance}" property="name"/>', [personInstance: personInstance])
-		
-		assert output == '<input type="text" name="name" value="Bartholomew Roberts" id="name" />'
-	}
+
+        def output = applyTemplate('<g:scaffoldInput bean="${personInstance}" property="password"/>', [personInstance: personInstance])
+
+        assert output == '<input type="password" name="password" value="BlackBart">'
+    }
+
+    // TODO: test for template provided by a plugin
+
+    void testResolvesConfiguredTemplateForDomainClassProperty() {
+
+    }
+
+    void testResolvesConfiguredTemplateForPropertyType() {
+        resourceLoader.registerMockResource("/templates/_date.gsp", '<input type="date" name="${property}" value="${formatDate(date: value, format: \'yyyy-MM-dd\')}">')
+
+        withConfig('scaffolding.template.default."java.util.Date" = "/templates/date"') {
+            println "In test: ${ga.config}"
+            def output = applyTemplate('<g:scaffoldInput bean="${personInstance}" property="dateOfBirth"/>', [personInstance: personInstance])
+
+            assert output == '<input type="date" name="dateOfBirth" value="1682-05-17">'
+        }
+    }
+
+    void testUsesDefaultRendering() {
+        def output = applyTemplate('<g:scaffoldInput bean="${personInstance}" property="name"/>', [personInstance: personInstance])
+
+        assert output == '<input type="text" name="name" value="Bartholomew Roberts" id="name" />'
+    }
 
 }
