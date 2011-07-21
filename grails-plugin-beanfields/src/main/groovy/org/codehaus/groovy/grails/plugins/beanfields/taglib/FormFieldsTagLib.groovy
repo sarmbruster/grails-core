8a19d25a0d3a3c145c52fb312c24dd74a6ce609d
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-07-21 08:34:03 +0100

    changed method ordering - would be nice to break default rendering code out to a separate class

--- grails-plugin-beanfields/src/main/groovy/org/codehaus/groovy/grails/plugins/beanfields/taglib/FormFieldsTagLib.groovy
+++ grails-plugin-beanfields/src/main/groovy/org/codehaus/groovy/grails/plugins/beanfields/taglib/FormFieldsTagLib.groovy
@@ -64,6 +64,52 @@ class FormFieldsTagLib implements GrailsApplicationAware {
 		return propertyAccessor
 	}
 
+	// TODO: cache the result of this lookup
+	private String resolveFieldTemplate(BeanPropertyAccessor propertyAccessor) {
+		// order of priority for template resolution
+		// 1: grails-app/views/controller/<property>/_field.gsp
+		// 2: grails-app/views/forms/<class>.<property>/_field.gsp
+		// 3: grails-app/views/forms/<type>/_field.gsp
+		// 4: grails-app/views/forms/default/_field.gsp
+		// TODO: implications for templates supplied by plugins
+		def templateResolveOrder = []
+		templateResolveOrder << GrailsResourceUtils.appendPiecesForUri("/", controllerName, propertyAccessor.propertyName, "field")
+		templateResolveOrder << GrailsResourceUtils.appendPiecesForUri("/forms", propertyAccessor.beanClass.propertyName, propertyAccessor.propertyName, "field")
+		templateResolveOrder << GrailsResourceUtils.appendPiecesForUri("/forms", propertyAccessor.type.name, "field")
+		templateResolveOrder << "/forms/default/field"
+
+		def template = templateResolveOrder.find {
+			groovyPageLocator.findTemplateByPath(it)
+		}
+		return template
+	}
+
+	private resolveBean(Map attrs) {
+		pageScope.variables[attrs.bean] ?: attrs.bean
+	}
+
+	private GrailsDomainClass resolveDomainClass(bean) {
+		resolveDomainClass(bean.getClass())
+	}
+
+	private GrailsDomainClass resolveDomainClass(Class beanClass) {
+		grailsApplication.getArtefact("Domain", beanClass.simpleName)
+	}
+
+	private Map<String, Object> resolveProperty(bean, String property) {
+		def domainClass = resolveDomainClass(bean.getClass())
+		def path = property.tokenize(".")
+		resolvePropertyFromPathComponents(PropertyAccessorFactory.forBeanPropertyAccess(bean), domainClass, path)
+	}
+
+	private String resolveLabelText(BeanPropertyAccessor propertyAccessor, Map attrs) {
+		def label = attrs.label
+		if (!label && attrs.labelKey) {
+			label = message(code: attrs.labelKey)
+		}
+		label ?: message(code: propertyAccessor.labelKey, default: propertyAccessor.defaultLabel)
+	}
+
 	private String renderInput(Map attrs) {
 		def model = [:]
 		model.name = attrs.property
@@ -172,52 +218,6 @@ class FormFieldsTagLib implements GrailsApplicationAware {
 		def addLabel = g.message(code: 'default.add.label', args: [referencedTypeLabel])
 		buffer << g.link(controller: controllerName, action: "create", params: [("${attrs.beanDomainClass.propertyName}.id".toString()): attrs.bean.id], addLabel)
 		buffer as String
-	}
-
-	// TODO: cache the result of this lookup
-	private String resolveFieldTemplate(BeanPropertyAccessor propertyAccessor) {
-		// order of priority for template resolution
-		// 1: grails-app/views/controller/<property>/_field.gsp
-		// 2: grails-app/views/forms/<class>.<property>/_field.gsp
-		// 3: grails-app/views/forms/<type>/_field.gsp
-		// 4: grails-app/views/forms/default/_field.gsp
-		// TODO: implications for templates supplied by plugins
-		def templateResolveOrder = []
-		templateResolveOrder << GrailsResourceUtils.appendPiecesForUri("/", controllerName, propertyAccessor.propertyName, "field")
-		templateResolveOrder << GrailsResourceUtils.appendPiecesForUri("/forms", propertyAccessor.beanClass.propertyName, propertyAccessor.propertyName, "field")
-		templateResolveOrder << GrailsResourceUtils.appendPiecesForUri("/forms", propertyAccessor.type.name, "field")
-		templateResolveOrder << "/forms/default/field"
-
-		def template = templateResolveOrder.find {
-			groovyPageLocator.findTemplateByPath(it)
-		}
-		return template
-	}
-
-	private resolveBean(Map attrs) {
-		pageScope.variables[attrs.bean] ?: attrs.bean
-	}
-
-	private GrailsDomainClass resolveDomainClass(bean) {
-		resolveDomainClass(bean.getClass())
-	}
-
-	private GrailsDomainClass resolveDomainClass(Class beanClass) {
-		grailsApplication.getArtefact("Domain", beanClass.simpleName)
-	}
-
-	private Map<String, Object> resolveProperty(bean, String property) {
-		def domainClass = resolveDomainClass(bean.getClass())
-		def path = property.tokenize(".")
-		resolvePropertyFromPathComponents(PropertyAccessorFactory.forBeanPropertyAccess(bean), domainClass, path)
-	}
-
-	private String resolveLabelText(BeanPropertyAccessor propertyAccessor, Map attrs) {
-		def label = attrs.label
-		if (!label && attrs.labelKey) {
-			label = message(code: attrs.labelKey)
-		}
-		label ?: message(code: propertyAccessor.labelKey, default: propertyAccessor.defaultLabel)
 	}
 
 }
